a56ea282bd3134b5aed8810688365220de8dbed3
Davi de Paula Coelho Raia dos Santos <davipernalonga30@gmail.com>
2022-05-07 02:33:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,31 +2,29 @@ from time import sleep
 from win32 import win32print, win32api
 import os
 
-
-
-
 lista_impressoras = win32print.EnumPrinters(2)
 for impressora in lista_impressoras:
     print(impressora)
 
-
-principal_imp = lista_impressoras[6]
+principal_imp = lista_impressoras[0]
 print(principal_imp[2])
 win32print.SetDefaultPrinter(principal_imp[2])
 
-
-a = int(input("Coloque a quantidade de cópias"))
-caminho = input("Cole o caminho(imprime tudo no diretório)")
-caminho = caminho
-
+a = int(input("Coloque a quantidade de cópias: "))
+caminho = input("Cole o caminho(imprime tudo no diretório) ")
 
 lista_arq = os.listdir(caminho)
 
 b = 1
 while b <= a:
     for arq in lista_arq:
+        print("enviando request número: ", b)
         win32api.ShellExecute(0, "print", arq, None, caminho, 0)
-        sleep(120)
-        print(a)
+        if b<a:
+            sleep(240)
+            print("metade do tempo, impressão: ", b)
+            sleep(240)
+            print("Acabou o tempo de espera, indo para a proxima impressao")
+        else:
+            print("Trabalho concluído")
     b += 1
-    
