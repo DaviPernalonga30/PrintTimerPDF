5e818c5a4200994dc3089f61659db3fb37cd31aa
Davi de Paula Coelho Raia dos Santos <davipernalonga30@gmail.com>
2022-05-06 22:13:53 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from time import sleep
+from win32 import win32print, win32api
+import os
+
+
+
+
+lista_impressoras = win32print.EnumPrinters(2)
+for impressora in lista_impressoras:
+    print(impressora)
+
+
+principal_imp = lista_impressoras[6]
+print(principal_imp[2])
+win32print.SetDefaultPrinter(principal_imp[2])
+
+
+a = int(input("Coloque a quantidade de cópias"))
+caminho = input("Cole o caminho")
+caminho = caminho
+
+
+lista_arq = os.listdir(caminho)
+
+b = 1
+while b <= a:
+    for arq in lista_arq:
+        win32api.ShellExecute(0, "print", arq, None, caminho, 0)
+        sleep(120)
+        print(a)
+    b += 1
+    
